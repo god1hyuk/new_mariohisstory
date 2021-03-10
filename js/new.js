4e9f72e530b809a81ff8901a6c4d7804e0d4eafd
wonhyukshin <wonhyuk6340@gmail.com>
2021-03-10 20:06:21 +0900

    v1.0
    20210310

---
+++ js/new.js
@@ -0,0 +1,25 @@
+let swiper1 = new Swiper(".swiper1", {
+  effect: "fade",
+  loop: true,
+  navigation: {
+    nextEl: ".nextBtn1",
+    prevEl: ".prevBtn1",
+  },
+});
+let swiper2 = new Swiper('.swiper1_txt', {
+  loop: true,
+  navigation: {
+    nextEl: '.nextBtn1',
+    prevEl: '.prevBtn1',
+  },
+});
+
+
+let swiper3 = new Swiper(".swiper2", {
+  effect: "fade",
+  loop: true,
+  navigation: {
+    nextEl: ".nextBtn2",
+    prevEl: ".prevBtn2",
+  },
+});
